1b5651a63adf82d14081e37c9a26b338d4713942
Oscar Mauricio Hevilla <mauhevilla@gmail.com>
2018-05-28 12:16:11 -0300

    se coloco el mapa en la pagina de inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @NgModule({
   declarations: [
@@ -23,7 +24,8 @@ import { MyApp } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    Geolocation
   ]
 })
 export class AppModule {}
--- src/pages/inicio/inicio.ts
+++ src/pages/inicio/inicio.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { Geolocation } from '@ionic-native/geolocation';
+import { Platform } from 'ionic-angular';
+
+
+declare var google: any;
+
 /**
  * Generated class for the InicioPage page.
  *
@@ -14,10 +20,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   templateUrl: 'inicio.html',
 })
 export class InicioPage {
+  map: any; // Manejador del mapa.
+  coords : any = { lat: 0, lng: 0 }
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController,
+              public navParams: NavParams,
+              public  platform: Platform,
+              private geolocation: Geolocation) {
+
+    platform.ready().then(() => {
+      // La plataforma esta lista y ya tenemos acceso a los plugins.
+         this.obtenerPosicion();
+    });
   }
 
+  obtenerPosicion():any{
+    this.geolocation.getCurrentPosition().then(res => {
+      this.coords.lat = res.coords.latitude;
+      this.coords.lng = res.coords.longitude;
+
+      this.loadMap();
+    })
+    .catch(
+      (error)=>{
+        console.log(error);
+      }
+    );
+  }
+  loadMap(){
+    let mapContainer = document.getElementById('map');
+     this.map = new google.maps.Map(mapContainer, {
+       center: this.coords,
+       zoom: 12
+     });
+ }
   ionViewDidLoad() {
     console.log('ionViewDidLoad InicioPage');
   }
